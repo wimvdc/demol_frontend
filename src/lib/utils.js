e3baff529f23ebf7cb548f71d00d57ff2d64faff
Wim <wim.vdc@hotmail.com>
2022-03-14 17:50:13 +0100

    restructure + auth fix

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -3,8 +3,12 @@ export const baseUrl = `${import.meta.env.VITE_PUBLIC_API_BASE_URL}`;
 export const getData = async (url) => {
 	const response = await fetch(`${baseUrl}${url}`, { credentials: 'include' });
 	if (!response.ok) {
-		const message = `An error has occured: ${response.status}`;
-		throw new Error(message);
+		if (response.status === 401) {
+			window.location.href = '/login';
+		} else {
+			const message = `An error has occured: ${response.status}`;
+			throw new Error(message);
+		}
 	}
 	return await response.json();
 };
@@ -50,29 +54,6 @@ export const putData = async (url, body) => {
 export const isRequestSuccesfull = async (url) => {
 	const response = await fetch(`${baseUrl}${url}`, { credentials: 'include' });
 	return response.ok;
-};
-
-export const getQueryVariable = (querystring, variable) => {
-	let vars = querystring.split('&');
-	for (let i = 0; i < vars.length; i++) {
-		let pair = vars[i].split('=');
-		if (decodeURIComponent(pair[0]) == variable) {
-			return decodeURIComponent(pair[1]);
-		}
-	}
-	return undefined;
-};
-
-export const urlBase64ToUint8Array = (base64String) => {
-	const padding = '='.repeat((4 - (base64String.length % 4)) % 4);
-	const base64 = (base64String + padding).replace(/\-/g, '+').replace(/_/g, '/');
-	const rawData = window.atob(base64);
-	const outputArray = new Uint8Array(rawData.length);
-
-	for (let i = 0; i < rawData.length; ++i) {
-		outputArray[i] = rawData.charCodeAt(i);
-	}
-	return outputArray;
 };
 
 export const preCandidates = [
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,56 +1,102 @@
-<script context="module" lang="ts">
-	export const prerender = true;
+<script>
+	import GameState from '$lib/components/GameState.svelte';
+	import { getData } from '$lib/utils';
+
+	async function getInfo() {
+		return await getData(`/v1/game/info?format=full`);
+	}
+
+	let info = getInfo();
 </script>
 
-<script lang="ts">
-</script>
-
-<svelte:head>
-	<title>Home</title>
-</svelte:head>
-
-<section>
-	<h1>
-		<div class="welcome">
-			<picture>
-				<source srcset="svelte-welcome.webp" type="image/webp" />
-				<img src="svelte-welcome.png" alt="Welcome" />
-			</picture>
+<div class="container">
+	<div class="tile is-ancestor">
+		<div class="tile is-vertical">
+			<div class="tile">
+				<div class="tile is-parent is-vertical">
+					<article class="tile is-child notification is-danger" style="text-align:center;">
+						{#await info}
+							<p class="title">Laden...</p>
+						{:then result}
+							<p class="title">Ronde #{result.round}</p>
+						{/await}
+					</article>
+					<article class="tile is-child notification is-warning">
+						{#await info}
+							Laden...
+						{:then result}
+							<GameState {...result} />
+						{/await}
+					</article>
+					{#await info then result}
+						{#if result.round === 7}
+							<article class="tile is-child notification is-primary" style="text-align:center;">
+								<p class="title">Finale spel!</p>
+								<div class="new">
+									Je hebt momenteel {result.spendable} punten verdiend.<br />
+									Deze laatste ronde verloopt anders dan voorgaande ronden:
+									<ul>
+										<li>
+											Je <a href="/endgame">kiest</a> deze keer enkel jouw Mol (zonder punten).
+										</li>
+										<li>
+											Op de <a href="/info">info</a> pagina vind je hoe eindscore zal worden berekend.
+										</li>
+										<li>We proberen tegen zondag 21u45 de definitieve score online te zetten.</li>
+									</ul>
+								</div>
+							</article>
+						{/if}
+					{/await}
+				</div>
+				<div class="tile is-parent">
+					<article class="tile is-child notification is-info">
+						<p class="title">Groepen</p>
+						<p class="subtitle">Je bent lid van volgende groepen</p>
+						<div>
+							{#await info}
+								Laden...
+							{:then result}
+								<ul class="groups">
+									{#each result.groups as item, i}
+										<li>
+											<a href="/group/{item.uuid}">
+												{item.name}
+											</a>
+										</li>
+									{/each}
+								</ul>
+							{/await}
+						</div>
+					</article>
+				</div>
+			</div>
 		</div>
+	</div>
+</div>
 
-		to your new<br />SvelteKit app
-	</h1>
+<style lang="scss">
+	ul.groups {
+		margin-left: 20px;
+		a {
+			text-decoration: none;
+		}
+	}
 
-	<h2>
-		try editing <strong>src/routes/index.svelte</strong>
-	</h2>
-</section>
-
-<style>
-	section {
+	.voting {
 		display: flex;
-		flex-direction: column;
-		justify-content: center;
 		align-items: center;
-		flex: 1;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
 		height: 100%;
-		top: 0;
-		display: block;
+	}
+
+	.new {
+		text-align: left;
+		ul {
+			margin-left: 20px;
+		}
+	}
+
+	ul {
+		list-style-type: disclosure-closed;
 	}
 </style>
