e7bd5bf63ab4c68b7252391ddaa9f2297d5f5f46
Wim <wim.vdc@hotmail.com>
2022-03-19 08:52:48 +0100

    cleanup

--- src/service-worker.js
+++ src/service-worker.js
@@ -7,11 +7,8 @@ const CACHE_NAME = `static-cache-${version}`;
 const to_cache = build.concat(files);
 
 worker.addEventListener('install', (event) => {
-	console.log('[ServiceWorker] Install');
-
 	event.waitUntil(
 		caches.open(CACHE_NAME).then((cache) => {
-			console.log('[ServiceWorker] Pre-caching offline page');
 			return cache.addAll(to_cache).then(() => {
 				worker.skipWaiting();
 			});
@@ -20,14 +17,12 @@ worker.addEventListener('install', (event) => {
 });
 
 worker.addEventListener('activate', (event) => {
-	console.log('[ServiceWorker] Activate');
 	// Remove previous cached data from disk
 	event.waitUntil(
 		caches.keys().then(async (keys) =>
 			Promise.all(
 				keys.map((key) => {
 					if (key !== CACHE_NAME) {
-						console.log('[ServiceWorker] Removing old cache', key);
 						return caches.delete(key);
 					}
 				})
@@ -38,7 +33,7 @@ worker.addEventListener('activate', (event) => {
 });
 
 self.addEventListener('fetch', (event) => {
-	console.log('[ServiceWorker] Fetch', event.request.url);
+	// console.log('[ServiceWorker] Fetch', event.request.url);
 	if (event.request.mode !== 'navigate') {
 		return;
 	}
