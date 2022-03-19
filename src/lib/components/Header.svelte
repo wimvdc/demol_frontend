0467ef0a4838bf29a50961ac76b24f76c06eded8
Wim <wim.vdc@hotmail.com>
2022-03-19 09:36:47 +0100

    fix for menu

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -7,28 +7,37 @@
 	import { baseUrl } from '$lib/utils';
 	export let isLoggedIn = true;
 
-	$: if (isLoggedIn) {
-		setTimeout(() => {
-			document.querySelectorAll('a.navbar-item').forEach((navitem) => {
-				navitem.addEventListener('click', (e) => {
-					document.getElementById('basicnavbar').classList.toggle('is-active');
-				});
-			});
-		}, 1000);
-	}
+	// $: if (isLoggedIn) {
+	// 	setTimeout(() => {
+	// 		document.querySelectorAll('a.navbar-item').forEach((navitem) => {
+	// 			navitem.addEventListener('click', (e) => {
+	// 				document.getElementById('basicnavbar').classList.toggle('is-active');
+	// 			});
+	// 		});
+	// 	}, 1000);
+	// }
 
-	document.addEventListener('DOMContentLoaded', () => {
-		const navbarBurgers = document.getElementById('menu');
-		navbarBurgers.addEventListener('click', () => {
-			document.getElementById(navbarBurgers.dataset.target).classList.toggle('is-active');
-			navbarBurgers.classList.toggle('is-active');
-		});
-	});
+	// document.addEventListener('DOMContentLoaded', () => {
+	// 	const navbarBurgers = document.getElementById('menu');
+	// 	navbarBurgers.addEventListener('click', () => {
+	// 		document.getElementById(navbarBurgers.dataset.target).classList.toggle('is-active');
+	// 		navbarBurgers.classList.toggle('is-active');
+	// 	});
+	// });
+	let showMobile = false;
+	const handleMobileIconClick = (event) => {
+		showMobile = !showMobile;
+		console.log('ok!');
+	};
+
+	const hide = () => {
+		showMobile = false;
+	};
 </script>
 
 <nav class="navbar" role="navigation" aria-label="main navigation">
 	<div class="navbar-brand">
-		<a class="navbar-item" href="/home" id="appname"> De Mol '22 </a>
+		<a class="navbar-item" href="/home" id="appname" on:click={hide}> De Mol '22 </a>
 		<!-- svelte-ignore a11y-missing-attribute -->
 		<a
 			id="menu"
@@ -38,25 +47,25 @@
 			aria-expanded="false"
 			data-target="basicnavbar"
 		>
-			<i class="fa fa-user-circle" />
+			<i class="fa fa-user-circle" on:click={handleMobileIconClick} />
 		</a>
 	</div>
 
-	<div id="basicnavbar" class="navbar-menu">
+	<div id="basicnavbar" class="navbar-menu" class:is-active={showMobile}>
 		<div class="navbar-start">
 			{#if isLoggedIn}
-				<a class="navbar-item" href="/game"> Mollen </a>
-				<a class="navbar-item" href="/group"> Groepen </a>
-				<div class="navbar-item has-dropdown is-hoverable">
-					<!--svelte-ignore a11y-missing-attribute-->
-					<a class="navbar-link"> Statistieken </a>
+				<a class="navbar-item" on:click={hide} href="/game"> Mollen </a>
+				<a class="navbar-item" on:click={hide} href="/group"> Groepen </a>
+				<!-- <div class="navbar-item has-dropdown is-hoverable"> -->
+				<!--svelte-ignore a11y-missing-attribute-->
+				<!-- <a class="navbar-link"> Statistieken </a>
 					<div class="navbar-dropdown">
-						<a class="navbar-item" href="/insight/me"> Mijn inzet </a>
-						<a class="navbar-item" href="/insight"> Iedereen zijn inzet </a>
-					</div>
-				</div>
+						<a class="navbar-item" on:click={hide} href="/insight/me"> Mijn inzet </a>
+						<a class="navbar-item" on:click={hide} href="/insight"> Iedereen zijn inzet </a>
+					</div> -->
+				<!-- </div> -->
 			{/if}
-			<a class="navbar-item" href="/info"> Info </a>
+			<a class="navbar-item" on:click={hide} href="/info"> Info </a>
 		</div>
 
 		<div class="navbar-end">
@@ -65,7 +74,7 @@
 					<!-- svelte-ignore a11y-missing-attribute -->
 					<a id="me" class="navbar-link is-arrowless"> <i class="fa-solid fa-user" /> </a>
 					<div class="navbar-dropdown is-right">
-						<a class="navbar-item" href="/profile"> Mijn Profiel </a>
+						<a class="navbar-item" on:click={hide} href="/profile"> Mijn Profiel </a>
 						<!--a class="navbar-item" href="/feedback"> Feedback </a-->
 						<a class="navbar-item" href="{baseUrl}/auth/v1/logout"> Uitloggen </a>
 					</div>
@@ -102,7 +111,7 @@
 
 	a.navbar-item,
 	i.fa-user {
-		color: #babcc0;
+		color: lightslategrey;
 		&:hover {
 			color: black;
 		}
